b34fe8101da921a63c828ca286081255f49438f7
sakuchichi <okugou.0729@gmail.com>
2019-01-09 18:00:20 +0900

    古いファイルを削除しました。

--- ConnectDb.php
+++
@@ -1,9 +0,0 @@
-<?php
-function ConnectDb(){
-    $dsn = 'mysql:dbname=money_manager; host=127.0.0.1; charset=utf8';
-    $usr = 'root';
-    $passwd = '';
-    //データベースへの接続を確立
-    $db = new PDO($dsn, $usr, $passwd);
-    return $db; 
-}
